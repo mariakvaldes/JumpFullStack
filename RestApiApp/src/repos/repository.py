17dab9614d2686e4c96dd69e540d4ff37b91590f
Maria K Valdes <mariavaldes003@gmail.com>
2026-07-14 12:29:24 -0400

    created the data model for customer

---
+++ RestApiApp/src/repos/repository.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+import os
+
+MONGO_DETAILS = "mongodb+srv://mariavaldes003_db_user:JQAuhLVWzOATZADr@cluster.5zvq2jl.mongodb.net/"
+
+client = AsyncIOMotorClient(MONGO_DETAILS)
+database = client.bank_db
+customer_collection = database.get_collection("customers")

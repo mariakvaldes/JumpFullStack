2b2835ddb1f1fe86744356815b2ab659ae3ee9c2
Maria K Valdes <mariavaldes003@gmail.com>
2026-07-14 23:44:20 -0400

    created service layer for accounts

---
+++ RestApiApp/src/services/account_service.py
@@ -0,0 +1,7 @@
+from src.repos.repository import account_collection
+from src.models.account import Account
+
+async def create_account(account: Account):
+    account_dict = account.model_dump(by_alias=True, exclude={"id"})
+    result = await account_collection.insert_one(account_dict)
+    return {"id": str(result.inserted_id), **account_dict}
